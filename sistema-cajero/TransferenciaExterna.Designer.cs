25cda3e3d50426cea732fc4e596d9ff0c6580e93
DanielVasquez19-bit <vp23006@ues.edu.sv>
2025-05-26 11:18:35 -0600

    cammbie la caja de texto de transferencias exxterna por una combo box con las opciones de banco xd

--- sistema-cajero/TransferenciaExterna.Designer.cs
+++ sistema-cajero/TransferenciaExterna.Designer.cs
@@ -36,8 +36,8 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.txtMonto = new System.Windows.Forms.TextBox();
-            this.txtBancoDestino = new System.Windows.Forms.TextBox();
             this.txtCuentaDestino = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,6 +47,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.Snow;
+            this.panel1.Controls.Add(this.comboBox1);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.btnPagar);
@@ -54,10 +55,9 @@
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.txtMonto);
-            this.panel1.Controls.Add(this.txtBancoDestino);
             this.panel1.Controls.Add(this.txtCuentaDestino);
             this.panel1.Location = new System.Drawing.Point(103, 19);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(393, 448);
             this.panel1.TabIndex = 11;
@@ -151,16 +151,6 @@
             this.txtMonto.TabIndex = 3;
             this.txtMonto.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMonto_KeyPress);
             // 
-            // txtBancoDestino
-            // 
-            this.txtBancoDestino.BackColor = System.Drawing.Color.White;
-            this.txtBancoDestino.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtBancoDestino.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBancoDestino.Location = new System.Drawing.Point(104, 145);
-            this.txtBancoDestino.Name = "txtBancoDestino";
-            this.txtBancoDestino.Size = new System.Drawing.Size(187, 32);
-            this.txtBancoDestino.TabIndex = 3;
-            // 
             // txtCuentaDestino
             // 
             this.txtCuentaDestino.BackColor = System.Drawing.Color.White;
@@ -172,13 +162,30 @@
             this.txtCuentaDestino.TabIndex = 3;
             this.txtCuentaDestino.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCuentaDestino_KeyPress);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Banco Agricola",
+            "Banco Cuscatlan",
+            "Banco Davivienda",
+            "Banco Hipotecario de El Salvador",
+            "Banco Promerica",
+            "Banco de Fomento Agropecuario ",
+            "Banco Atlantida"});
+            this.comboBox1.Location = new System.Drawing.Point(105, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(185, 30);
+            this.comboBox1.TabIndex = 4;
+            // 
             // TransferenciaExterna
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 490);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "TransferenciaExterna";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TransferenciaExterna";
@@ -197,7 +204,7 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox txtMonto;
-        private System.Windows.Forms.TextBox txtBancoDestino;
         private System.Windows.Forms.TextBox txtCuentaDestino;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
